ac449726a717770e86b41a44ed27c5114860cd9d
Lance Amolo <lanceamolo808@yahoo.com>
2022-06-01 18:52:27 -0700

    m1 05 done

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -6,5 +6,29 @@ public class DecimalToBinary {
 
 	public static void main(String[] args) {
 
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String valueInput = userInput.nextLine();
+
+		String[] numbersArr = valueInput.split(" ");
+
+
+		// binary = (decimal % 2) + binary;
+		for (int i = 0; i < numbersArr.length; i++) {
+			int decimal = Integer.parseInt(numbersArr[i]);
+
+			String binary = "";
+			for (; decimal >= 0; decimal = decimal / 2) {
+				binary = (decimal % 2) + binary;
+				if (decimal == 0) {
+					break;
+				}
+
+			}
+			System.out.println(numbersArr[i] + " in binary is " + binary);
+
+		}
+
 	}
 }
--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,32 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter the length: ");
+		String lengthInput = userInput.nextLine();
+		int length = Integer.parseInt(lengthInput);
+
+		System.out.print("Is the measurement in (m)eter, or (f)eet?: ");
+		String metricInput = userInput.nextLine();
+
+		//m = f * 0.3048 feet to meters
+		//f = m * 3.2808399 meters to feet
+		//58f is 17m.
+
+		if("f".equals(metricInput)){
+			int meterConvert = (int) (length * 0.3048);
+			System.out.println(length + metricInput + " is " + meterConvert + "m.");
+		} else if ("m".equals((metricInput))){
+			int feetConvert = (int) (length * 3.2808399);
+			System.out.println(length + metricInput + " is " + feetConvert + "f.");
+		} else {
+			System.out.println("Input is invalid. Please try again!");
+		}
 
 	}
 
