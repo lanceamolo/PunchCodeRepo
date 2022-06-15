365bc6d997fc17d383834305c02f4cb2a91a2daf
Lance Amolo <lanceamolo808@yahoo.com>
2022-06-15 13:51:08 -0700

    061522

--- module-1/12_Polymorphism/exercise/src/main/java/com/techelevator/BankCustomer.java
+++ module-1/12_Polymorphism/exercise/src/main/java/com/techelevator/BankCustomer.java
@@ -1,11 +1,44 @@
 package com.techelevator;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class BankCustomer implements Accountable{
+public class BankCustomer{
 
     private String name;
     private String address;
     private String phoneNumber;
     private List<Accountable> accounts = new ArrayList<Accountable>();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Accountable[] getAccounts(){
+        return accounts.toArray(new Accountable[accounts.size()]);
+    }
+
+    public void addAccount(Accountable newAccount){
+        accounts.add(newAccount);
+    }
 }
